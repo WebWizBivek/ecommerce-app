9af73fbdf4682db465b155255a3bab12836add31
bivek <bivekwangkhem6001@gmail.com>
2025-04-03 15:26:21 +0530

    refactor Loader component styles; integrate Loader in SignIn page for loading state management

--- frontend/src/customCom/Loader.jsx
+++ frontend/src/customCom/Loader.jsx
@@ -16,22 +16,22 @@ const Loader = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black bg-opacity-80 backdrop-blur-md">
-      <div className="relative mb-6">
+    <div className="flex flex-col  items-center justify-center h-screen w-full bg-white/10 backdrop-blur-xl">
+      <div className=" mb-6 p-4  flex flex-col items-center backdrop-blur-sm rounded-2xl shadow-md bg-white/50">
         <img
           src="/logo.jpg"
           alt="Loading..."
-          className="w-40 h-40 object-contain rounded-full animate-spin-slow"
+          className="w-20 h-20 object-contain rounded-full animate-pulse shadow-md"
         />
-        <div className="absolute inset-0 flex items-center justify-center"></div>
+        <p>Pitch and Thread</p>
       </div>
-      <div className="w-64 h-3 bg-gray-700 rounded-full overflow-hidden shadow-lg">
+      <div className="w-64 h-3 bg-gray-300/60 rounded-full overflow-hidden shadow-md">
         <div
-          className="h-full bg-gradient-to-r from-red-500 to-yellow-500 transition-all duration-300"
+          className="h-full bg-gradient-to-r from-blue-500 to-green-500 rounded-full transition-all duration-300 shadow-md"
           style={{ width: `${progress}%` }}
         ></div>
       </div>
-      <p className="text-white mt-4 font-bold text-lg tracking-wide animate-pulse">
+      <p className="text-gray-900 mt-4 font-semibold text-lg tracking-wide animate-pulse drop-shadow-md">
         {progress}% Loading...
       </p>
     </div>
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import axios from "axios";
+import Loader from "@/customCom/Loader";
 // import { Link } from "react-router";
 
 export default function SignIn() {
@@ -13,59 +14,74 @@ export default function SignIn() {
   const [loading, setLoading] = useState(false);
 
   const handleSignIn = async () => {
-    const response = await axios.post(`${BASE_URL}/user/signin`, {
-      email,
-      password,
-    });
-    2;
+    try {
+      setLoading(true);
+      const response = await axios.post(`${BASE_URL}/user/signin`, {
+        email,
+        password,
+      });
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center  p-6 bg-[url(https://media.istockphoto.com/id/2163983591/vector/abstract-multicolored-gradient-vector-background-design-wallpaper-template-with-geometric.jpg?s=612x612&w=0&k=20&c=wb1CT6XfnlzyhA63kMaN6LW0EM-ltGJSPSPlSW9qHos=)] bg-no-repeat bg-center bg-cover">
-      <Card className="w-full max-w-md shadow-2xl rounded-2xl p-8 bg-white text-gray-900 ">
-        <CardHeader className="text-center flex flex-col items-center">
-          <img src="/logo.jpg" alt="logo" className="rounded-full h-20 w-20" />
-          <p className="text-black text-sm mt-2 font-bold">Pitch and Thread</p>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-6">
-            <div>
-              <Label className="text-gray-700">Email</Label>
-              <Input
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="mt-1 border-gray-300 bg-gray-100 focus:ring-2 focus:ring-pink-500 rounded-xl p-3"
-              />
-            </div>
-            <div>
-              <Label className="text-gray-700">Password</Label>
-              <Input
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="mt-1 border-gray-300 bg-gray-100 focus:ring-2 focus:ring-pink-500 rounded-xl p-3"
-              />
-            </div>
-            <Button
-              className="w-full mt-4 bg-gradient-to-r from-pink-500 to-red-500 hover:from-red-500 hover:to-pink-500 text-white font-bold py-3 px-4 rounded-xl shadow-lg transition duration-300"
-              onClick={handleSignIn}
-              disabled={loading}
-            >
-              Signin
-            </Button>
+    <>
+      {loading && <Loader />}
 
-            <p className="text-sm text-center text-gray-600 mt-4">
-              Don't have an account?{" "}
-              {/* <Link to="/signup" className="text-pink-500 font-bold">
+      <div className="flex min-h-screen items-center justify-center  p-6 bg-[url(https://media.istockphoto.com/id/2163983591/vector/abstract-multicolored-gradient-vector-background-design-wallpaper-template-with-geometric.jpg?s=612x612&w=0&k=20&c=wb1CT6XfnlzyhA63kMaN6LW0EM-ltGJSPSPlSW9qHos=)] bg-no-repeat bg-center bg-cover">
+        <Card className="w-full max-w-md shadow-2xl rounded-2xl p-8 bg-white text-gray-900 ">
+          <CardHeader className="text-center flex flex-col items-center">
+            <img
+              src="/logo.jpg"
+              alt="logo"
+              className="rounded-full h-20 w-20"
+            />
+            <p className="text-black text-sm mt-2 font-bold">
+              Pitch and Thread
+            </p>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-6">
+              <div>
+                <Label className="text-gray-700">Email</Label>
+                <Input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="mt-1 border-gray-300 bg-gray-100 focus:ring-2 focus:ring-pink-500 rounded-xl p-3"
+                />
+              </div>
+              <div>
+                <Label className="text-gray-700">Password</Label>
+                <Input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mt-1 border-gray-300 bg-gray-100 focus:ring-2 focus:ring-pink-500 rounded-xl p-3"
+                />
+              </div>
+              <Button
+                className="w-full mt-4 bg-gradient-to-r from-pink-500 to-red-500 hover:from-red-500 hover:to-pink-500 text-white font-bold py-3 px-4 rounded-xl shadow-lg transition duration-300"
+                onClick={handleSignIn}
+                disabled={loading}
+              >
+                Signin
+              </Button>
+
+              <p className="text-sm text-center text-gray-600 mt-4">
+                Don't have an account?{" "}
+                {/* <Link to="/signup" className="text-pink-500 font-bold">
                 Sign Up
               </Link> */}
-            </p>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
